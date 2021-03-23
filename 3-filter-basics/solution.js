2b49598d5201a61a60a526264ce31ee8b3e1af74
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-23 12:54:01 +0100

    3 done

--- 3-filter-basics/solution.js
+++ 3-filter-basics/solution.js
@@ -1,3 +1,13 @@
 const grades = [5, 1, 1.3, 3.7, 1.7, 2.1, 2.4, 4.1, 6];
 
 // Your code below
+
+const lowerGrades = grades.filter((grade) => {
+  return grade < 3;
+});
+console.log(lowerGrades);
+
+const higherGrades = grades.filter((grade) => {
+  return grade >= 3;
+});
+console.log(higherGrades);
