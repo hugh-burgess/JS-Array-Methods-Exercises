b513af138c4e0f03a7825403b49f11536bf069b4
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-23 14:26:11 +0100

    5 done

--- 5-map-basics/solution.js
+++ 5-map-basics/solution.js
@@ -1,3 +1,18 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // Your code below
+
+const double = numbers.map((number) => {
+  return number * 2;
+});
+console.log(double);
+
+const divide = numbers.map((number) => {
+  return number / 2;
+});
+console.log(divide);
+
+const multiply = numbers.map((number) => {
+  return number * number;
+});
+console.log(multiply);
